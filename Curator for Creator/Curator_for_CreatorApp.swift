5944f3a4eec706825f74a13565a647174902a990
Mahdi BND <mahdi.bnd.7@gmail.com>
2023-05-11 15:35:13 +0330

    Initial Commit

---
+++ Curator for Creator/Curator_for_CreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curator_for_CreatorApp.swift
+//  Curator for Creator
+//
+//  Created by Mahdi BND on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Curator_for_CreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
